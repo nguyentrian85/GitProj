b7496f811747e02dbfa9040a80d03705136d01a1
nguyentrian85 <nguyentrian85@gmail.com>
2013-09-20 11:49:46 +0700

    Revert "sa"
    This reverts commit 92a2074380110434da32c3677751b3f45da8034b.

---
+++ GitProj/src/com/an/nguyen/TestGit.java
@@ -0,0 +1,7 @@
+package com.an.nguyen;
+
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+}
--- GitProj/src/com/git/local/Test2.java
+++ GitProj/src/com/git/local/Test2.java
@@ -1,7 +1,5 @@
 package com.git.local;
 
 public class Test2 {
-	public static void main(String[] args) {
-		
-	}
+ass
 }
